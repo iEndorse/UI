9633108b0d0bf6b466333e5bae74884c9867be8c
Christian Okonta <okonta.christian@yahoo.com>
2024-05-16 13:11:09 +0000

    feat:Added signuppage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,13 +5,14 @@ import { LandingPageGuestUser1Component } from "./landing-page/landing-page-gues
 import { LandingPageGuestUser2Component } from "./landing-page/landing-page-guest-user2/landing-page-guest-user2.component";
 import { LandingPageGuestUser4Component } from "./landing-page/landing-page-guest-user4/landing-page-guest-user4.component";
 import { SignUpComponent } from "./signup-authentication/sign-up/sign-up.component";
+import { BankDetails1Component } from './earnings-wallet/bank-details1/bank-details1.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [RouterOutlet, CommonModule, RouterLink, LandingPageGuestUser1Component, LandingPageGuestUser2Component, LandingPageGuestUser4Component, SignUpComponent]
+    imports: [RouterOutlet, CommonModule, RouterLink, LandingPageGuestUser1Component, LandingPageGuestUser2Component, LandingPageGuestUser4Component, SignUpComponent, BankDetails1Component]
 })
 export class AppComponent {
   title = 'iEndorse';
